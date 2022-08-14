dc3c36f33fe68102beccb7874e9d16ccba7a20e2
patcard21 <71289158+patcard21@users.noreply.github.com>
2022-08-14 15:16:09 -0400

    Added main compo. to App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
+import React from "react";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">
+          <a
+            className="App-link"
+            href="https://github.com/patcard21/dictionary-react-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub Repository
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
