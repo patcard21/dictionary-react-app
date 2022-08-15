37b0c099b98bb674cee1de8160fe6805550fdd0a
patcard21 <71289158+patcard21@users.noreply.github.com>
2022-08-15 13:27:30 -0400

    Created loops in Meaning component
    ...in order to display all definition entries.

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -16,7 +16,7 @@ export default function Dictionary() {
 
     // Documentation: https://dictionaryapi.dev/
 
-    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
 
--- src/Meaning.js
+++ src/Meaning.js
@@ -4,8 +4,19 @@ export default function Meaning(props) {
   console.log(props.meaning);
   return (
     <div className="Meaning">
-      {" "}
       <h3>{props.meaning.partOfSpeech}</h3>
+      {props.meaning.definitions.map(function (definition, index) {
+        return (
+          <div key={index}>
+            <p>
+              {definition.definition}
+              <br />
+              <br />
+              <em> {definition.example} </em>
+            </p>
+          </div>
+        );
+      })}
       <p>{props.meaning.definitions[0].definition}</p>
       <p>{props.meaning.definitions[0].example}</p>
     </div>
