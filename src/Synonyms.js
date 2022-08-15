2a93002de44a45f9b3460e64393907c7a9f0271b
patcard21 <71289158+patcard21@users.noreply.github.com>
2022-08-15 16:06:07 -0400

    Updated the apiUrl

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -1,13 +1,14 @@
 import React from "react";
 
 export default function Synonyms(props) {
+  console.log(props.synonyms);
   if (props.synonyms) {
     return (
-      <ul className="Synonyms">
+      <div className="Synonyms">
         {props.synonyms.map(function (synonym, index) {
           return <li key={index}>{synonym}</li>;
         })}
-      </ul>
+      </div>
     );
   } else {
     return null;
