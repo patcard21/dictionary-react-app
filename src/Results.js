760b0294311e93b4a8d08bd95056dda652604177
patcard21 <71289158+patcard21@users.noreply.github.com>
2022-08-15 09:42:53 -0400

    Used props and loop to get the dicito. results from api

--- src/Results.js
+++ src/Results.js
@@ -1,8 +1,15 @@
 import React from "react";
 
 export default function Results(props) {
-  if (props.results.length) {
-    return <div className="Results">Hello from Results</div>;
+  if (props.results) {
+    return (
+      <div className="Results">
+        <h2>{props.results.word}</h2>
+        {props.results.meanings.map(function (meaning, index) {
+          return meaning.definitions[0].definition;
+        })}
+      </div>
+    );
   } else {
     return null;
   }
