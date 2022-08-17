79a600c0e76e32277248853313c18c0958a97e0c
patcard21 <71289158+patcard21@users.noreply.github.com>
2022-08-17 11:22:23 -0400

    Added phonetic audio samples to show when avail

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -6,6 +6,11 @@ export default function Phonetic(props) {
   console.log(props.phonetic);
   return (
     <div className="Phonetic">
+      {props.phonetic.audio && (
+        <a href={props.phonetic.audio} rel="noreferrer" target="_blank">
+          Listen
+        </a>
+      )}
       <span className="text">{props.phonetic.text}</span>
     </div>
   );
